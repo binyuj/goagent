d5ffe0d1c915f854ebd3454c8ba28e254f5d1760
binyuj <yubinlove1991@gmail.com>
2012-12-17 23:17:11 +0800

    google.cn is back

--- local/proxy.py
+++ local/proxy.py
@@ -992,7 +992,7 @@ def gaeproxy_handler(sock, address, hls={'setuplock':gevent.coros.Semaphore()}):
             if common.GAE_PROFILE == 'google_cn':
                 with hls['setuplock']:
                     if common.GAE_PROFILE == 'google_cn':
-                        hosts = ('www.google.co.jp', 'www.google.com.tw')
+                        hosts = ('www.google.cn', 'www.g.cn')
                         iplist = []
                         for host in hosts:
                             try:
